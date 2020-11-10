5d9128a60e218b5f09d4bd9e10d0de49ae2c3f98
David Chatting <david@davidchatting.com>
2020-11-10 13:06:02 +0000

    ethbroadcast already defined for ESP8266

--- src/Approximate/eth_addr.h
+++ src/Approximate/eth_addr.h
@@ -14,10 +14,9 @@
 
 #elif defined(ESP32)
     #include "lwip/etharp.h"
-
+    const struct eth_addr ethbroadcast = {{0xff,0xff,0xff,0xff,0xff,0xff}};
+    
 #endif
-
-const struct eth_addr ethbroadcast = {{0xff,0xff,0xff,0xff,0xff,0xff}};
 
 #ifndef ETHADDR16_COPY
   #define ETHADDR16_COPY(src, dst)  SMEMCPY(src, dst, ETHARP_HWADDR_LEN)
